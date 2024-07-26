7f2e7d7a9873b6a6ac6cf28eaa24c1483ab28c1c
FrancoisHUP <francois.h.marcoux@gmail.com>
2024-07-26 11:20:26 -0400

    init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Demos
+
+1. pip install -r requirements.txt
+2. scrapy startproject web_crawler
+3. You can change the "start_urls" @web_crawler/web_crawler/spiders/web_crawler_spider.py
+4. scrapy crawl prompting -o output.json
+Bonus. chek for "DEPTH_LIMIT" to controle how depth the web_crawler cna follow links 
---
+++ web_crawler/web_crawler/spiders/web_crawler_spider.py
@@ -0,0 +1,30 @@
+import scrapy
+from bs4 import BeautifulSoup
+
+class WebCrawlerSpider(scrapy.Spider):
+    name = "web_crawler"
+    start_urls = ["https://linum-lab.ca/"]
+
+    def parse(self, response):
+        # Use BeautifulSoup to parse the HTML content
+        soup = BeautifulSoup(response.body, 'lxml')
+
+        # Extract text content from the parsed HTML
+        page_text = soup.get_text(separator=' ', strip=True)
+
+        # Store the extracted text
+        yield {
+            'url': response.url,
+            'text': page_text,
+        }
+
+        # Follow links to other pages and repeat the process
+        for link in response.css('a::attr(href)').getall():
+            if link.startswith('/'):
+                link = response.urljoin(link)
+            yield scrapy.Request(link, callback=self.parse)
+
+        # Handle pagination
+        next_page = response.css('a.next::attr(href)').get()
+        if next_page:
+            yield response.follow(next_page, self.parse)
